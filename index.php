0da30dde4c4f7648cda464a7a01e98bb334d79ca
Morgocik <git@danilowicz.info>
2016-10-02 02:53:45 +0200

    gówno kod

--- index.php
+++ index.php
@@ -15,11 +15,41 @@ $level = (isset($_POST['level']) ? $_POST['level'] : 'a2');
 //$offset = (isset($_GET['offset']) ? $_GET['offset'] : 0);
 //$newOffset = $offset + $tableRows;
 
-$results = $dbConnection->executeQuery("SELECT * FROM `irregular_verbs` WHERE `level`='$level'");
+$results = $dbConnection->executeQuery("SELECT * FROM `irregular_verbs` WHERE `level` = '$level'");
 //var_dump($results);
 //exit();
 
+$results_sorted = [];
+foreach ($results as $result) {
+    $results_sorted[ $result['id'] ] = $result;
+    $results_sorted[ $result['id'] ]['status'] = '';
+}
 
+if (isset($_POST['action']) && $_POST['action'] == 'check') {
+    foreach ($_POST['test'] as $verb) {
+        $isValid = false;
+        if (isset($results_sorted[$verb['id']])) {
+            switch (true) {
+                case isset($verb['infinitive']):
+                    if ($results_sorted[$verb['id']]['infinitive'] == $verb['infinitive']) {
+                        $isValid = true;
+                    }
+                    break;
+                case isset($verb['past_tense']):
+                    if ($results_sorted[$verb['id']]['past_tense'] == $verb['past_tense']) {
+                        $isValid = true;
+                    }
+                    break;
+                case isset($verb['past_participle']):
+                    if ($results_sorted[$verb['id']]['past_participle'] == $verb['past_participle']) {
+                        $isValid = true;
+                    }
+                    break;
+            }
+        }
+        $results_sorted[$verb['id']]['status'] = $isValid ? 'true' : 'false';
+    }
+}
 
 
 include 'Views/main_page.php';
